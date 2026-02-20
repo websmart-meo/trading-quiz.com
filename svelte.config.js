2554f81dcc1439a816e27ae30bcb47267651fd34
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2026-02-20 20:24:42 +0300

    fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/trading-quiz.com' : ''
+		}
+	}
+};
 
 export default config;
